924c2df1de1a3dfde372c8931b76c9c2cf8a8e86
PrikazchikovDA <danilprikipkf@gmail.com>
2022-05-20 07:35:20 +0300

    (Task 11-12.2)

---
+++ getLineByNumber/getLineByNumber/getLineByNumber.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string read_line_n(const std::string& name, int n) {
+    std::string  line;
+    std::ifstream f(name);
+
+    while (f.eof() != 1 && (n > 0)) {
+        getline(f, line);
+        --n;
+    }
+
+    return line;
+}
+
+
+
+int main(void) {
+    std::string filename;
+    std::cin >> filename;
+    std::string line = read_line_n("some_rows.txt", 2);
+    std::cout << line << std::endl;
+}
