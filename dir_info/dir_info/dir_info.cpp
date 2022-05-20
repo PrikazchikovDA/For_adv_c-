bb45615cb532fdad99b15533cb74a1aec0c82d25
PrikazchikovDA <danilprikipkf@gmail.com>
2022-05-20 07:35:57 +0300

    (Task 11-12.3)

---
+++ dir_info/dir_info/dir_info.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main()
+{
+    // Что такое тип изменения и какую функцию для этого нужно вызвать, я не нашел :-)
+    std::string path;
+    std::cin >> path;
+    for (const auto& entry : fs::directory_iterator(path))
+        std::cout << entry.path()
+        << " | " << entry.last_wtite_time() 
+        << std::endl;
+}
